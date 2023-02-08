ea4903b473f5c2f17c4868f89dcba91da7b33da8
Chethan M K <Chethan M K@LAPTOP-FQFCD50K>
2023-02-08 11:02:12 +0530

    code missing

---
+++ Tunds/src/chethau/Chethan.java
@@ -0,0 +1,21 @@
+package chethau;
+
+public class Chethan 
+package advance;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chrome_launch 
+{
+	public static void main(String[] args)
+	{
+		String key="webdriver.chrome.driver";
+		String value="./softwares/chromedriver.exe";
+		System.setProperty(key, value);
+		//ChromeDriver driver=new ChromeDriver();
+	}
+
+
+}
+
+
